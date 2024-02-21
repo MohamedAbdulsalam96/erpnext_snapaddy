050e96f73ecbfd2b230ddde772511bfd34052296
barredterra <14891507+barredterra@users.noreply.github.com>
2024-02-21 20:03:28 +0100

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## snapADDY Integration
+
+Leads von snapADDY zu ERPNext synchronisieren
+
+#### License
+
+GPLv3
---
+++ erpnext_snapaddy/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "snapADDY Integration",
+			"type": "module",
+			"label": _("snapADDY Integration")
+		}
+	]
